5e4b1805a67c281c5878c0632d10ce62420efc7a
Kricskovics Máté <elfoagdod@gmail.com>
2023-06-26 17:12:17 +0200

    b3

--- mario.js
+++ mario.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-=======
-function cheer(team, color) {
-    console.log(team);
-    console.log(color);
-}
 
-cheer('magenta', 'magenta');
->>>>>>> b634456bc83c265dacfcc23991026611e02aefba
